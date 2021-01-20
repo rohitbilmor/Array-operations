ece5690923a7ece66794279695cd6096ba3eb911
Bandela Rohith Kumar <rohitbilmore1234@gmail.com>
2021-01-20 23:22:15 +0530

    Add files via upload

---
+++ Sum_all_ele_array.java
@@ -0,0 +1,15 @@
+package com.src1;
+
+public class Sum_all_ele_array {
+
+	public static void main(String[] args) {
+		int sum=0;
+		int A[]= {1,2,3,4,5,6};
+		for(int i=0;i<=A.length-1;i++)
+		{
+			sum=sum+A[i];
+		}
+			System.out.println(sum);
+	}
+
+}
