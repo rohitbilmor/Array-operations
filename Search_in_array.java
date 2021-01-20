961e07c2a95eb7915514ee36f95bc33d9a4e4a09
Bandela Rohith Kumar <rohitbilmore1234@gmail.com>
2021-01-20 23:33:12 +0530

    Update Search_in_array.java

--- Search_in_array.java
+++ Search_in_array.java
@@ -1,9 +1,12 @@
+//we can search an element from an array which prints the index of key. 
 package com.src1;
-
+import java.util.Scanner;
 public class Search_in_array {
 
 	public static void main(String[] args) {
-		int s=6;
+		Scanner sc=new Scanner(System.in);
+		int s;
+		s=sc.nextInt();
 		int A[]= {1,2,3,4,5,6};
 		for(int i=0;i<=A.length-1;i++)
 		{
