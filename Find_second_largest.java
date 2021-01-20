b915ea84ee384e2e03a9ee605115791a79c8643e
Bandela Rohith Kumar <rohitbilmore1234@gmail.com>
2021-01-20 23:33:46 +0530

    Add files via upload

---
+++ Find_second_largest.java
@@ -0,0 +1,26 @@
+package com.src1;
+
+public class Find_second_largest {
+
+	public static void main(String[] args) {
+		int max1,max2;
+		int A[]= {5,6,4,7,8,12,14,15,16};
+		max1=A[0];
+		max2=A[0];
+		for(int i=0;i<=A.length-1;i++)
+		{
+			if(A[i]>max1)
+			{
+				max2=max1;
+				max1=A[i];
+			}
+			else if(A[i]>max2)
+			{
+				max2=max1;
+			}
+		}
+		System.out.println("Second largest is "+max2);
+
+	}
+
+}
